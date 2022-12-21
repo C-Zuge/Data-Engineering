22f8568303e613b5f9f3d1aedb0c4392ab6fbbc0
Cesar Zuge <cesar.vinicius@pontomais.com.br>
2022-12-21 13:28:42 -0300

    Enhancing build and recreation on docker files and examples docs

--- airflow/build_environment.sh
+++ airflow/build_environment.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 # Build the base images from which are based the Dockerfiles
-# then Startup all the containers at once
-docker-compose up airflow-init && \
-docker-compose up -d
+# then Startup all the containers at once deatached
+docker-compose up -d --build --force-recreate
